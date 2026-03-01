906613db94745b250be14c87c511afdae34e50f7
Irakli <irakli.guraspashvili@nga911.com>
2026-03-01 22:10:02 +0400

    refactor(modules): Removed app controller and service, Added new health module and endpoint in modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { PrismaModule } from "./core/prisma/prisma.module";
+import { HealthCheckModule } from "./modules/health-check/health-check.module";
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [ConfigModule.forRoot(), PrismaModule, HealthCheckModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
