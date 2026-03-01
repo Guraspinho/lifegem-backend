11a56285c5c810c92151d459cf4b844064c99340
Irakli <irakli.guraspashvili@nga911.com>
2026-03-01 21:55:53 +0400

    docs(core): Added swagger docs configuration and health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Health Check")
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@ApiOperation({ 
+        summary: "Application Health Check",
+        description: "Pings the server to verify it is actively accepting requests." 
+    })
+	@ApiResponse({
+		description: 'The server is healthy and running.',
+		example: { health: true },
+	})
+	@Get("/health-check")
+	getHealthStatus(): { health: true } {
+		return this.appService.getHealthStatus();
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule } from "@nestjs/swagger";
+import { swaggerConfig } from "./core/swagger/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup("api-docs", app, swaggerDocument);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
